67efe9a5704c4b30050dcb5eeb836422f4a0a5ec
liulian26 <breadlv326@gmail.com>
2025-03-19 19:38:07 -0700

    fix: remove unnecessary Router component and update package-lock.json formatting

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,15 @@
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
 import "./styles/Global.css";
 
 
 function App() {
-
-
   return (
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </Router>
-  )
+    <Routes>
+      <Route path='/' element={<Home />} />
+    </Routes>
+  );
 }
 
 export default App
