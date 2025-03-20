88454a828c984301540cf745edb89748db68762d
liulian26 <breadlv326@gmail.com>
2025-03-19 19:56:31 -0700

    feat: wrap App component in StrictMode and fetch artifacts in Home component

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
+import { BrowserRouter } from 'react-router-dom';
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-     <App />
-  </BrowserRouter>
-)
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import "../styles/Home.css";
@@ -7,6 +7,17 @@ import "../styles/Home.css";
 import bannerImage from "../assets/images/banner.png";
 
 const Home = () => {
+
+  const [artifacts, setArtifacts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/api/artifacts')
+      .then(res => res.json())
+      .then(data => setArtifacts(data))
+      .catch(err => console.error(err));
+  }, []);
+
+
   return (
     <>
       <Navbar />
@@ -18,6 +29,17 @@ const Home = () => {
             <Link to="/DunhuangMuseum" className="learn-more-btn">Learn More</Link>
           </div>
         </div>
+      </div>
+
+      {/* Artifacts */}
+      <div className="artifacts-container">
+        {artifacts.map(artifact => (
+          <div key={artifact._id} className="artifact-card">
+            <h3>{artifact.title}</h3>
+            <img src={artifact.images[0]} alt={artifact.title} style={{width: '200px'}} />
+            <p>{artifact.description}</p>
+          </div>
+        ))}
       </div>
     </>
   );
