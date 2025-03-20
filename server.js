1b705f4b70e5b28f513dd1e4e572805e6a448add
saraxixi <saraxi.xixi@gmail.com>
2025-03-20 14:20:25 -0700

    feat: implement models and routes for artifacts, caves, exhibitions, and users

--- server.js
+++ server.js
@@ -1,23 +1,22 @@
-import express from 'express'
-import mongoose from 'mongoose'
-import cors from 'cors'
-import dotenv from 'dotenv'
-
-import userRoutes from './routes/userRoutes.js'
-import artifactRoute from './routes/artifactRoutes.js'
-import caveRoutes from './routes/caveRoutes.js'
-import exhibitionRoutes from './routes/exhibitionRoutes.js'
-
-
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 dotenv.config();
+
+import userRoutes from './src/routes/userRoutes.js'
+import artifactRoute from './src/routes/artifactRoutes.js'
+import caveRoutes from './src/routes/caveRoutes.js'
+import exhibitionRoutes from './src/routes/exhibitionRoutes.js'
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 
 // Connect to MongoDB
-const MONGODB_URI = process.env.MONGODB_URI
-mongoose.connect(MONGODB_URI, {
+const uri = process.env.MONGODB_URI
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -27,11 +26,9 @@ mongoose.connect(MONGODB_URI, {
   .catch((err) => {
     console.error('MongoDB connection error:', err)
   })
-  
-  console.log("MongoDB URI:", process.env.MONGODB_URI);
 
 
-// // test: GET /api/test
+// Test route
 app.get('/', (req, res) => {
   res.send('Hello from Dunhuang Digital Museum backend!')
 })
@@ -43,13 +40,13 @@ app.use('/api/users', userRoutes);
 app.use('/api/exhibitions', exhibitionRoutes);
 
 // Error handler
-app.use((err, req, res, next) => {
+app.use((err, req, res) => {
   console.error(err.stack)
   res.status(500).json({ error: err.message })
 });
 
 // Start server
-// const PORT = process.env.PORT || 4000
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`)
-// });
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`)
+});
