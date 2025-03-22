f2237822f0bf619c479e7d08871a0c7e1b50f717
saraxixi <saraxi.xixi@gmail.com>
2025-03-21 23:19:44 -0700

    feat: add login functionality with user authentication and create login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import {Routes, Route} from 'react-router-dom'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import "./styles/Global.css";
 
 
@@ -7,6 +8,7 @@ function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
     </Routes>
   );
 }
--- src/models/User.js
+++ src/models/User.js
@@ -22,6 +22,7 @@ const userSchema = new mongoose.Schema({
     enum: ['user', 'admin'],
     default: 'user'
   },
+  
   // the artifacts that the user has saved...
   savedArtifacts: [
     {
