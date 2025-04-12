70be95af0d68788c8fc5d65ff9f78a35c3e6559d
Shuojun Chen <shuojunchen@gmail.com>
2025-04-12 11:00:36 -0700

    go back to an earlier version and get back all mural data

--- server/src/seeds/muralData.js
+++ server/src/seeds/muralData.js
@@ -238,6 +238,198 @@ export const muralData = [
     rating: 4,
     location: "East wall of the main chamber, Mogao Cave 012",
     images: ["/assets/images/scarf-dance3.png"]
+  },
+  {
+    title: "Four-Gate Tower",
+    location: "South wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 5,
+    images: ["/assets/images/four-gate-tower.png"]
+  },
+  {
+    title: "Round Single-Story Brick and Stone Tower",
+    location: "South wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 5,
+    images: ["/assets/images/round-brick-tower.png"]
+  },
+  {
+    title: "Round Single-Story Brick and Stone Tower",
+    location: "South wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 5,
+    images: ["/assets/images/round-stone-tower.png"]
+  },
+  {
+    title: "Western City",
+    location: "South wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 5,
+    images: ["/assets/images/western-city.png"]
+  },
+  {
+    title: "Expanded Palace",
+    location: "South wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 5,
+    images: ["/assets/images/expanded-palace.png"]
+  },
+  {
+    title: "Chinese and Western Style Residences",
+    location: "South wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/chinese-western-residences.png"]
+  },
+  {
+    title: "Four-Tiered Platform",
+    location: "North wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/four-tiered-platform.png"]
+  },
+  {
+    title: "City Gate and City Walls",
+    location: "North wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/city-gate-walls.png"]
+  },
+  {
+    title: "Detailed Parts of Decorated Rafters",
+    location: "North wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/decorated-rafters.png"]
+  },
+  {
+    title: "Ducks and Geese on the Balustrades",
+    location: "North wall of the main chamber, Mogao Cave 217",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/balustrade-birds.png"]
+  },
+  {
+    title: "Large Dougong Bracketing System",
+    location: "South wall of the main chamber, Mogao Cave 061",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/dougong-brackets.png"]
+  },
+  {
+    title: "Pure Land Temple",
+    location: "South wall of the main chamber, Mogao Cave 061",
+    category: "mural",
+    subcategory: "architecture",
+    rating: 4,
+    images: ["/assets/images/pure-land-temple.png"]
+  },
+  {
+    title: "Musical Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 285",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 5,
+    images: ["/assets/images/musical-apsaras.png"]
+  },
+  {
+    title: "Round Single-Body Child Flying Apsaras",
+    location: "Brick and stone tower, South wall of the main chamber, Mogao Cave 285",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 5,
+    images: ["/assets/images/child-apsaras.png"]
+  },
+  {
+    title: "Golden-Haired Flying Apsaras",
+    location: "North wall of the main chamber, Mogao Cave 003",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 5,
+    images: ["/assets/images/golden-haired-apsaras.png"]
+  },
+  {
+    title: "Flying Apsaras",
+    location: "North wall of the main chamber, Mogao Cave 003",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 5,
+    images: ["/assets/images/flying-apsaras1.png"]
+  },
+  {
+    title: "Flower Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 003",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 5,
+    images: ["/assets/images/flower-apsaras1.png"]
+  },
+  {
+    title: "Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 003",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/flying-apsaras2.png"]
+  },
+  {
+    title: "Scattered Flower Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 103",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/scattered-flower-apsaras1.png"]
+  },
+  {
+    title: "Spreading Wings Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 103",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/spreading-wings-apsaras.png"]
+  },
+  {
+    title: "Rising Dance Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 112",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/rising-dance-apsaras1.png"]
+  },
+  {
+    title: "Rising Dance Flying Apsaras",
+    location: "South wall of the main chamber, Mogao Cave 112",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/rising-dance-apsaras2.png"]
+  },
+  {
+    title: "Flying Apsaras in Pure Land",
+    location: "North wall of the main chamber, Mogao Cave 321",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/pure-land-apsaras.png"]
+  },
+  {
+    title: "Scattered Flower Flying Apsaras",
+    location: "North wall of the main chamber, Mogao Cave 321",
+    category: "mural",
+    subcategory: "flying apsaras",
+    rating: 4,
+    images: ["/assets/images/scattered-flower-apsaras2.png"]
   }
 ];
 
